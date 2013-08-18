0f2b3660ab7e375a47493a67c48e8e7b166cb326
mayurkhatri <mayurkhatri122@gmail.com>
2013-08-18 04:31:36 -0700

    contacts - index and new

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,29 @@
 class ContactsController < ApplicationController
-  
   def index
-    @contacts = Contact.all
+    @contacts = current_user.contacts.all
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.user = current_user
+    if @contact.save
+      flash[:success] = 'Contact created successfully'
+      redirect_to user_contacts_path(current_user.id) 
+    else
+      flash[:error] = 'Contact could not be saved'
+      render action: 'new'
+    end
+  end
+
+  def new
+    @user = current_user
+    @contact = Contact.new
   end
 
   def destroy
     @contact = Contact.find_by_id(params[:id])
     @contact.destroy
     flash[:notice] = 'Contact successfully deleted'
-    redirect_to action: 'index'
-  end
-
-  def new
-    @user = User.find_by_id(params[:user_id])
-    @contact = Contact.new
+    redirect_to action: 'index' 
   end
 end

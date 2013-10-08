8d34fd2918e1fd94d4fbb00be18cfca76783341a
mayurkhatri <mayur@cuberoot.in>
2013-10-08 16:01:46 +0530

    Devise - Reset password(recoverable) settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,17 @@ UserContacts::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'gmail.com',
+    :user_name => 'user@gmail.com',
+    :password => 'secret'
+  }
 end
